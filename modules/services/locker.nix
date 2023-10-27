d57c17000eafc4188fea9d6d0a7cba517c924b65
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2023-10-27 08:39:34 +0200

    modules: services: locker: fix command
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- modules/services/locker.nix
+++ modules/services/locker.nix
@@ -9,7 +9,7 @@
       services.screen-locker = {
         enable = true;
         inactiveInterval = 15;
-        lockCmd = "${pkgs.i3lock-color}/bin/i3lock-color --color=ffa500 --clock --show-failed-attempts --bar-indicator --date-str='%A %Y-%m-%d -i $(${pkgs.coreutils}/bin/shuf -n1 -e /home/risson/.lock-images/*.jpg)";
+        lockCmd = "${pkgs.i3lock-color}/bin/i3lock-color --color=ffa500 --clock --show-failed-attempts --bar-indicator -i $(${pkgs.coreutils}/bin/shuf -n1 -e /home/risson/.lock-images/*.jpg)";
       };
     })
   ];
