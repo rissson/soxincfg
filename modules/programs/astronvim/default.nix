243558c8f307206a8be7f88132493451a25731fe
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2024-04-21 15:41:35 +0200

    modules: programs: astronvim: update
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- modules/programs/astronvim/default.nix
+++ modules/programs/astronvim/default.nix
@@ -16,14 +16,15 @@
 
         cargo
         ripgrep
+        ruff
       ];
 
       xdg.configFile."nvim" = {
         source = pkgs.fetchFromGitHub {
           owner = "AstroNvim";
           repo = "AstroNvim";
-          rev = "v3.10.3";
-          sha256 = "sha256-AFWiB947LWww/PMRaMDx5c8UE++0Gnav2tgUx6TsQK0=";
+          rev = "v3.40.3";
+          sha256 = "sha256-h019vKDgaOk0VL+bnAPOUoAL8VAkhY6MGDbqEy+uAKg=";
         };
         # needed to have the custom user config
         recursive = true;
