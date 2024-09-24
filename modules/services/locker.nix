dccbd97ce15947c3f59024532e5109cf2655bb2e
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2024-09-24 18:03:32 +0200

    services/locker: black lockscreen
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- modules/services/locker.nix
+++ modules/services/locker.nix
@@ -8,8 +8,9 @@
     (lib.optionalAttrs (mode == "home-manager") {
       services.screen-locker = {
         enable = true;
-        inactiveInterval = 15;
-        lockCmd = "${pkgs.i3lock-color}/bin/i3lock-color --color=ffa500 --clock --show-failed-attempts --bar-indicator -i $(${pkgs.coreutils}/bin/shuf -n1 -e /home/risson/.lock-images/*.jpg)";
+        inactiveInterval = 10;
+        lockCmd = "${pkgs.i3lock-color}/bin/i3lock-color --color=000000 --clock --show-failed-attempts --bar-indicator";
+        # lockCmd = "${pkgs.i3lock-color}/bin/i3lock-color --color=ffa500 --clock --show-failed-attempts --bar-indicator -i $(${pkgs.coreutils}/bin/shuf -n1 -e /home/risson/.lock-images/*.jpg)";
       };
     })
   ];
