34eb854c669ba969efeed31407e488f343910a09
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2024-09-29 18:56:27 +0200

    modules/services/printing: add scanner udev rule
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- modules/services/printing.nix
+++ modules/services/printing.nix
@@ -11,6 +11,10 @@
         brscan5.enable = true;
       };
 
+      services.udev.extraRules = ''
+        ATTRS{idVendor}=="04f9", ATTRS{idProduct}=="0459", MODE="0664", GROUP="scanner", ENV{libsane_matched}="yes"
+      '';
+
       services.printing = {
         enable = true;
         drivers = [
