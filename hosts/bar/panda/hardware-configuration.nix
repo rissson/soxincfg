6b5b4a9d7934a5dfbbfc29d0d9da40f3d02070ae
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2026-06-12 15:50:41 +0200

    bar/panda: fix /boot permissions
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- hosts/bar/panda/hardware-configuration.nix
+++ hosts/bar/panda/hardware-configuration.nix
@@ -162,7 +162,7 @@
     "/boot" = {
       device = "/dev/disk/by-uuid/7680-42AD";
       fsType = "vfat";
-      options = ["fmask=0022" "dmask=0022"];
+      options = ["fmask=0077" "dmask=0077"];
     };
   };
 }
