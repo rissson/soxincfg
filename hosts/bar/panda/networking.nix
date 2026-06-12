311837e6894ad35e256e033f493904c984b93adb
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2026-06-12 15:50:09 +0200

    bar/panda: update networking
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- hosts/bar/panda/networking.nix
+++ hosts/bar/panda/networking.nix
@@ -25,6 +25,7 @@
         linkConfig.RequiredForOnline = "routable";
         networkConfig = {
           DHCP = "ipv4";
+          LinkLocalAddressing = "ipv6";
           IPv6AcceptRA = true;
         };
       };
@@ -32,9 +33,24 @@
         matchConfig.Name = "lan-mgmt-0";
         linkConfig.RequiredForOnline = "routable";
         networkConfig = {
-          DHCP = "ipv4";
+          LinkLocalAddressing = "ipv6";
           IPv6AcceptRA = true;
         };
+        address = ["172.28.2.225/27"];
+        routes = [
+          {
+            Destination = "172.28.0.0/16";
+            Gateway = "172.28.2.254";
+          }
+          {
+            Destination = "172.29.0.0/16";
+            Gateway = "172.28.2.254";
+          }
+          {
+            Destination = "209.112.97.0/24";
+            Gateway = "172.28.2.254";
+          }
+        ];
       };
     };
   };
